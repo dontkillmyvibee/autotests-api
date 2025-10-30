8607333c6a7e86fcaeb4b8fe77d604e1cac8f36a
dontkillmyvibee <lysorryoops@gmail.com>
2025-10-30 20:14:58 +0300

    6.2 Практикуемся в работе с HTTPX

---
+++ httpx_delete_user.py
@@ -0,0 +1,34 @@
+import email
+
+import httpx
+from tools.fakers import get_random_email
+
+create_user_payload = {
+    "email": get_random_email(),
+    "password": "user",
+    "lastName": "user",
+    "firstName": "user",
+    "middleName": "user"
+}
+
+create_user_response = httpx.post("http://localhost:8000/api/v1/users", json=create_user_payload)
+create_user_response_data = create_user_response.json()
+
+print(f"Status code: {create_user_response.status_code}")
+print(create_user_response_data)
+
+login_payload = {
+    "email": create_user_payload["email"],
+    "password": create_user_payload["password"]
+}
+login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+print(f"Status code: {login_response.status_code}")
+print(login_response_data)
+
+delete_user_headers = {
+    "Authorization": f"Bearer {login_response_data["token"]["accessToken"]}"
+}
+delete_user_response = httpx.delete(f"http://localhost:8000/api/v1/users/{create_user_response_data["user"]["id"]}",
+                                    headers=delete_user_headers)
+print(f"Status code: {delete_user_response.status_code}")
