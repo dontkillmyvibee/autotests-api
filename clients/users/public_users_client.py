af1dd419073e8f01115a7199bcdc184519dd4139
dontkillmyvibee <lysorryoops@gmail.com>
2025-11-04 17:11:23 +0300

    6.5 Практикуемся в использовании API клиентов

--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -1,10 +1,26 @@
 from typing import TypedDict
-import httpx
+
+from httpx import Response
+
 from clients.api_client import APIClient
+from clients.public_http_builder import get_public_http_client
 
 
-class CreateUserRequest(TypedDict):
-    """Типизированная структура запроса на создание пользователя"""
+class User(TypedDict):
+    """
+    Описание структуры пользователя.
+    """
+    id: str
+    email: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+
+class CreateUserRequestDict(TypedDict):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
     email: str
     password: str
     lastName: str
@@ -12,26 +28,36 @@ class CreateUserRequest(TypedDict):
     middleName: str
 
 
+class CreateUserResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: User
+
+
 class PublicUsersClient(APIClient):
     """
-    Клиент для публичных методов, не требующих авторизации
     Клиент для работы с /api/v1/users
     """
 
-    def create_user_api(self, request: CreateUserRequest) -> httpx.Response:
+    def create_user_api(self, request: CreateUserRequestDict) -> Response:
         """
-        Создаёт нового пользователя через публичный API.
+        Метод создает пользователя.
 
-        Args:
-            request (CreateUserRequest): Данные пользователя для создания.
-                Должны содержать поля:
-                - email (str)
-                - password (str)
-                - lastName (str)
-                - firstName (str)
-                - middleName (str)
-
-        Returns:
-            httpx.Response: Ответ сервера после выполнения запроса.
+        :param request: Словарь с email, password, lastName, firstName, middleName.
+        :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post("/api/v1/users", json=request)
+
+    def create_user(self, request: CreateUserRequestDict) -> CreateUserResponseDict:
+        response = self.create_user_api(request)
+        return response.json()
+
+
+def get_public_users_client() -> PublicUsersClient:
+    """
+    Функция создаёт экземпляр PublicUsersClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию PublicUsersClient.
+    """
+    return PublicUsersClient(client=get_public_http_client())
