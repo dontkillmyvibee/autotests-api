308e03952d8aa90d55b570753812f6ed9c4c5dbc
dontkillmyvibee <lysorryoops@gmail.com>
2025-11-04 22:07:07 +0300

    Практика написания Pydantic-моделей

--- pydantic_create_user.py
+++ pydantic_create_user.py
@@ -8,22 +8,22 @@ NameStr = Annotated[str, constr(min_length=1, max_length=50)]
 
 class UserSchema(BaseModel):
     """Модель данных пользователя"""
-    id: UUID = Field(...)
-    email: EmailStr = Field(..., min_length=1, max_length=250)
-    last_name: NameStr = Field(..., alias="lastName")
-    first_name: NameStr = Field(..., alias="firstName")
-    middle_name: NameStr = Field(..., alias="middleName")
+    id: UUID
+    email: EmailStr = Field(min_length=1, max_length=250)
+    last_name: NameStr = Field(alias="lastName")
+    first_name: NameStr = Field(alias="firstName")
+    middle_name: NameStr = Field(alias="middleName")
 
 
 class CreateUserRequestSchema(BaseModel):
     """Модель запроса на создание пользователя"""
-    email: EmailStr = Field(..., min_length=1, max_length=250)
-    password: constr(min_length=1, max_length=250) = Field(...)
-    last_name: NameStr = Field(..., alias="lastName")
-    first_name: NameStr = Field(..., alias="firstName")
-    middle_name: NameStr = Field(..., alias="middleName")
+    email: EmailStr = Field(min_length=1, max_length=250)
+    password: constr(min_length=1, max_length=250)
+    last_name: NameStr = Field(alias="lastName")
+    first_name: NameStr = Field(alias="firstName")
+    middle_name: NameStr = Field(alias="middleName")
 
 
 class CreateUserResponseSchema(BaseModel):
     """Модель ответа с данными созданного пользователя"""
-    user: UserSchema = Field(...)
+    user: UserSchema
