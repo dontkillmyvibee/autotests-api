fb2b165a18b5787a4ec490d6bb60738c63028dd0
dontkillmyvibee <lysorryoops@gmail.com>
2025-11-09 16:22:43 +0300

    Практика работы с pytest маркировками

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -10,6 +10,7 @@ from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 
+
 @pytest.mark.regression
 @pytest.mark.authentication
 def test_login():
