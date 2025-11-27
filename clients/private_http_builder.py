4492c7dbf92afce190b2c5f920986dfa27f36506
dontkillmyvibee <lysorryoops@gmail.com>
2025-11-27 18:53:22 +0300

    10.4 cURL в Allure-отчете: упрощаем анализ API-тестов

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -3,11 +3,13 @@ from pydantic import BaseModel
 from functools import lru_cache
 from clients.authentication.authentication_client import get_authentication_client
 from clients.authentication.authentication_schema import LoginRequestSchema
+from clients.event_hooks import curl_event_hook
 
 
 class AuthenticationUserSchema(BaseModel, frozen=True):
     email: str
     password: str
+
 
 @lru_cache(maxsize=None)
 def get_private_http_client(user: AuthenticationUserSchema) -> Client:
@@ -25,5 +27,6 @@ def get_private_http_client(user: AuthenticationUserSchema) -> Client:
     return Client(
         timeout=100,
         base_url="http://localhost:8000",
-        headers={"Authorization": f"Bearer {login_response.token.access_token}"}
+        headers={"Authorization": f"Bearer {login_response.token.access_token}"},
+        event_hooks={"request": [curl_event_hook]}
     )
