1c03a8cbe4269185290b3214a03c2f026ad0e63f
dontkillmyvibee <lysorryoops@gmail.com>
2025-12-01 14:31:32 +0300

    Практика работы с настройками

--- clients/files/files_schema.py
+++ clients/files/files_schema.py
@@ -1,3 +1,4 @@
+import pydantic
 from pydantic import BaseModel, HttpUrl, Field
 from tools.fakers import fake
 
@@ -17,7 +18,7 @@ class CreateFileRequestSchema(BaseModel):
     """
     filename: str = Field(default_factory=lambda: f"{fake.uuid4()}.png")
     directory: str = Field(default="tests")
-    upload_file: str
+    upload_file: pydantic.FilePath
 
 
 class CreateFileResponseSchema(BaseModel):
--- fixtures/files.py
+++ fixtures/files.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 
 from clients.files.files_client import get_files_client, FilesClient
 from clients.files.files_schema import CreateFileRequestSchema, CreateFileResponseSchema
+from config import settings
 from fixtures.users import UserFixture
 
 
@@ -10,13 +11,14 @@ class FileFixture(BaseModel):
     request: CreateFileRequestSchema
     response: CreateFileResponseSchema
 
+
 @pytest.fixture
 def files_client(function_user: UserFixture) -> FilesClient:
     return get_files_client(function_user.authentication_user)
 
+
 @pytest.fixture
 def function_file(files_client: FilesClient):
-    request = CreateFileRequestSchema(upload_file="./testdata/files/images.png")
+    request = CreateFileRequestSchema(upload_file=settings.test_data.image_png_file)
     response = files_client.create_file(request=request)
     return FileFixture(request=request, response=response)
-
